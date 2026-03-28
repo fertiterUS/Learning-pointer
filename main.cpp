93b94780ebab701aba33dc706281469f19004b45
fertiterUS <fyy2010128@gmail.com>
2026-03-28 13:05:31 -0700

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+
+
+int main() {
+	int a = 3;
+	int b = 4;
+
+	int* p = &a;
+	int* q = &b;
+
+	p = q;
+	*q = 10;
+	*p = 20;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "b: " << b << std::endl;
+}
